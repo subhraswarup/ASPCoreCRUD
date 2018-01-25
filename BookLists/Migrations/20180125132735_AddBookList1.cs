7a9fcde2aa1d927788fb8e08e04aa651159ceb7f
subhra swarup <subhra swarup@DESKTOP-IQVI4D6>
2018-01-25 19:16:39 +0530

    push

---
+++ BookLists/Migrations/20180125132735_AddBookList1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookLists.Migrations
+{
+    public partial class AddBookList1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "catId",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "catId",
+                table: "Books");
+        }
+    }
+}
